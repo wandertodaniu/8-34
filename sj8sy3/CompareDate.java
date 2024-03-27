35e1143797a19392dbb28253b63a9f3d0b13a25a
he <heyu@tianzhun.com>
2024-03-27 11:22:42 +0800

    8-34 one commit

---
+++ sj8sy3/CompareDate.java
@@ -0,0 +1,39 @@
+package sj8sy3;
+
+import java.util.*;
+public class CompareDate {
+   public static void main(String args[ ]) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("输入第一个年，月，日，时间数据");
+      System.out.print("输入年份");
+      short yearOne = scanner.nextShort();
+      System.out.print("输入月份");
+      byte monthOne = scanner.nextByte();
+      System.out.print("输入日期");
+      byte dayOne = scanner.nextByte();
+      System.out.println("输入第二个年，月，日，时间数据");
+      System.out.print("输入年份");
+      short yearTwo = scanner.nextShort();
+      System.out.print("输入月份");
+      byte monthTwo= scanner.nextByte();
+      System.out.print("输入日期");
+      byte dayTwo = scanner.nextByte();
+      Calendar calendar=null ;
+      calendar=calendar.getInstance();//��ʼ����������
+      calendar.set(yearOne,monthOne-1,dayOne);  //��calendar��ʱ������ΪyearOne��monthOne��dayOne��
+      long timeOne =calendar.getTimeInMillis();     //calendar��ʾ��ʱ��ת���ɺ���
+      calendar.set(yearTwo,monthTwo-1,dayTwo); 
+      long timeTwo=calendar.getTimeInMillis();
+      Date date1 = new Date(timeOne);       // ��timeOne����������date1
+      Date date2 =new Date(timeTwo);
+      if(date2.equals(date1))
+          System.out.println("两个日期的年、月、日完全相容");
+      else if(date2.after(date1))
+          System.out.println("您输入的第二个日期大于第一个日期");
+      else if(date2.before(date1))
+          System.out.println("您输入的第二个日期小鱼第一个日期");
+      long days=Math.abs(timeTwo-timeOne)/(1000*60*60*24);//ʹ��timeTwo,timeOne�������������������
+      System.out.println(yearOne+"年"+monthOne+"月"+dayOne+"日和"
+                         +yearTwo+"年"+monthTwo+"月"+dayTwo+"相隔"+days+"天");
+   }  
+}
---
+++ sj8sy4/HandleBigInteger.java
@@ -0,0 +1,35 @@
+package sj8sy4;
+
+import java.math.*;
+public class HandleBigInteger {
+   public static void main(String args[]) {
+      BigInteger n1=new BigInteger("987654321987654321987654321"),
+                 n2=new BigInteger("123456789123456789123456789"),
+                 result=null;
+       result=n1.add(n2);//n1��n2���ӷ�����
+       System.out.println("和："+result.toString());
+       result=n1.subtract(n2);//n1��n2����������
+       System.out.println("差:"+result.toString());
+       result=n1.multiply(n2);//n1��n2���˷�����
+       System.out.println("积:"+result.toString());
+       result=n1.divide(n2);//n1��n2����������
+       System.out.println("商:"+result.toString());
+       for(int i=1;BigInteger.valueOf(i)!=n1;i++){
+           result=BigInteger.valueOf(i*(i+1));
+       }
+       System.out.println("阶乘:"+result.toString());
+       BigInteger m=new BigInteger("17637"),
+                  COUNT=new BigInteger("0"),
+                  ONE=new BigInteger("1"),
+                  TWO=new BigInteger("2");
+      System.out.println(m.toString()+"的因子有：");
+      for(BigInteger i=TWO;i.compareTo(m)<0;i=i.add(ONE)) {
+          if((n1.remainder(i).compareTo(BigInteger.ZERO))==0) {
+              COUNT=COUNT.add(ONE);
+              System.out.print(" "+i.toString());
+          }
+      }
+      System.out.println("");
+      System.out.println(m.toString()+"一共有"+COUNT.toString()+"个因子");
+   }
+}
